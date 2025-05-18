a59d15f6f183342d099415add27c5a82cf084a3f
Andres Becerra <andbema@hotmail.com>
2025-05-18 03:16:06 -0600

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig,envField } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
-
+import Critters from 'critters';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
@@ -18,12 +18,38 @@ export default defineConfig({
     validateSecrets: false
   },
   session: {
-    // The name of the Unstorage driver
     driver: 'redis',
-    // The required options depend on the driver
     options: {
       url: process.env.REDIS_URL,
     },
-    ttl: 3600, // 1 hour
+    ttl: 3600,
+  },
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: undefined, // importante para que Critters funcione correctamente
+        },
+      },
+    },
+    plugins: [
+      {
+        name: 'vite-plugin-critters',
+        enforce: 'post',
+        apply: 'build',
+        async generateBundle(_, bundle) {
+          const critters = new Critters({
+            preload: 'swap',
+          });
+
+          for (const file of Object.keys(bundle)) {
+            const chunk = bundle[file];
+            if (chunk.type === 'asset' && chunk.fileName.endsWith('.html')) {
+              chunk.source = await critters.process(chunk.source);
+            }
+          }
+        },
+      },
+    ],
   },
 });
